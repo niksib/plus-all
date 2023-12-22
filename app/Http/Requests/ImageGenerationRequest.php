c401dd673f9900438c05b7103e057190e66df9ad
niksib <niksib@mbp-niksib.home>
2023-12-22 17:11:28 -0500

    update

--- app/Http/Requests/ImageGenerationRequest.php
+++ app/Http/Requests/ImageGenerationRequest.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Requests;
 
+use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
 
 class ImageGenerationRequest extends FormRequest
 {
@@ -16,9 +18,9 @@ class ImageGenerationRequest extends FormRequest
         return true;
     }
 
-    public function response(array $errors) {
-
-        return response()->json($errors, 403);
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json($validator->errors(), 422));
     }
 
     /**
@@ -29,14 +31,14 @@ class ImageGenerationRequest extends FormRequest
     public function rules()
     {
         return [
-            'name' => 'required|string|max:30',
-            'bg-color' => 'required|string|max:10',
-            'border-color' => 'required|string|max:10',
+            'name' => 'required|max:30',
+            'bg-color' => 'required|max:10',
+            'border-color' => 'required|max:10',
             'image' => 'required|image',
-            'width' => 'required|string|max:5',
-            'height' => 'required|string|max:5',
-            'phone' => 'required|email|max:30',
-            'email' => 'required|string|max:30',
+            'width' => 'required|max:5',
+            'height' => 'required|max:5',
+            'email' => 'required|email|max:30',
+            'phone' => 'required|max:30',
         ];
     }
 }
