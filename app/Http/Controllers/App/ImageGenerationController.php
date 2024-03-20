abf129ea1712c556fcb01e7346c45076efaa2ebf
niksib <niksib@mbp-niksib.home>
2024-03-20 14:31:25 -0400

    update

--- app/Http/Controllers/App/ImageGenerationController.php
+++ app/Http/Controllers/App/ImageGenerationController.php
@@ -33,7 +33,7 @@ class ImageGenerationController extends Controller
     ): JsonResponse {
         try {
             $imageGenerationService = new ImageGenerationService();
-            dd($request->file('image')->getClientMimeType(), $request->file('image')->getClientOriginalExtension());
+
             $data = [
                 'name' => $request->get('name'),
                 'bg_color' => $request->get('bg-color'),
